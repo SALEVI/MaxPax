0f1683f8954d64f629fe61191727944e60738de9
salevi <aleventesalcudean@gmail.com>
2024-05-16 12:37:32 +0300

    Changed category images

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -6,7 +6,9 @@ import { ActivityIndicator, ImageBackground, Pressable, Text, TextInput, View } 
 import { useAuth } from '~/providers/AuthProvider';
 import { supabase } from '~/utils/supabase';
 
-const background = { uri: 'https://w.wallhaven.cc/full/72/wallhaven-72oomo.jpg' };
+const background = {
+  uri: '*******/storage/v1/object/sign/images/loginBG12.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnZXMvbG9naW5CRzEyLnBuZyIsImlhdCI6MTcxNTg1MDg2NiwiZXhwIjoxNzQ3Mzg2ODY2fQ.6Xcr2tsObQCZ0rncdYZ_lnEMZOahh1PApH8raW4c5bs&t=2024-05-16T09%3A14%3A04.777Z',
+};
 
 const SignInScreen = () => {
   const [email, setEmail] = useState('');
@@ -33,63 +35,68 @@ const SignInScreen = () => {
 
   return (
     <ImageBackground source={background} resizeMode="cover" className="flex-1">
-      <View className="flex-1 justify-center dark:bg-black/55">
-        <Text className="mb-5 pl-5 text-4xl font-extrabold antialiased dark:text-zinc-200">
-          Log in
-        </Text>
-        <BlurView
-          intensity={30}
-          tint="dark"
-          experimentalBlurMethod="dimezisBlurView"
-          className="mx-5 flex h-96 justify-center rounded-2xl"
-          style={{ overflow: 'hidden' }}>
-          <View className=" w-full items-center justify-center">
-            <TextInput
-              value={email}
-              onChangeText={setEmail}
-              numberOfLines={1}
-              cursorColor="#84cc16"
-              keyboardType="email-address"
-              autoComplete="email"
-              placeholder="Enter your email address"
-              className="my-5 min-h-16 w-11/12 rounded-lg border border-lime-500 pl-4 text-lg font-semibold dark:bg-zinc-200 dark:text-zinc-900"
-            />
-            <TextInput
-              secureTextEntry
-              value={password}
-              onChangeText={setPassword}
-              numberOfLines={1}
-              cursorColor="#84cc16"
-              autoComplete="password"
-              placeholder="Enter your password"
-              className="min-h-16 w-11/12 rounded-lg border border-lime-500 pl-4 text-lg font-semibold dark:bg-zinc-200 dark:text-zinc-900"
-            />
-            <Text className="text-red-500" style={{ marginVertical: error !== '' ? 20 : 0 }}>
-              {error}
-            </Text>
+      <BlurView
+        intensity={15}
+        className="flex-1"
+        experimentalBlurMethod="none"
+        tint="systemChromeMaterial">
+        <View className="flex-1 justify-center dark:bg-black/60">
+          <Text className="mb-5 pl-5 text-4xl font-extrabold antialiased dark:text-zinc-200">
+            Log in
+          </Text>
+          <BlurView
+            intensity={30}
+            tint="systemThickMaterialDark"
+            experimentalBlurMethod="dimezisBlurView"
+            className="mx-5 flex h-96 justify-center rounded-2xl"
+            style={{ overflow: 'hidden' }}>
+            <View className=" w-full items-center justify-center">
+              <TextInput
+                value={email}
+                onChangeText={setEmail}
+                numberOfLines={1}
+                cursorColor="#84cc16"
+                keyboardType="email-address"
+                autoComplete="email"
+                placeholder="Enter your email address"
+                className="my-5 min-h-16 w-11/12 rounded-lg border border-lime-500 pl-4 text-lg font-semibold dark:bg-zinc-200 dark:text-zinc-900"
+              />
+              <TextInput
+                secureTextEntry
+                value={password}
+                onChangeText={setPassword}
+                numberOfLines={1}
+                cursorColor="#84cc16"
+                autoComplete="password"
+                placeholder="Enter your password"
+                className="min-h-16 w-11/12 rounded-lg border border-lime-500 pl-4 text-lg font-semibold dark:bg-zinc-200 dark:text-zinc-900"
+              />
+              <Text className="text-red-500" style={{ marginVertical: error !== '' ? 20 : 0 }}>
+                {error}
+              </Text>
 
-            <Pressable
-              disabled={loading}
-              onPress={signInWithEmail}
-              className="mb-5 min-h-16 w-11/12 items-center justify-center rounded-lg font-bold dark:bg-lime-500">
-              {loading ? (
-                <ActivityIndicator size="large" color="white" />
-              ) : (
-                <Text className="text-2xl font-extrabold antialiased dark:text-zinc-200">
-                  Sign in
-                </Text>
-              )}
-            </Pressable>
+              <Pressable
+                disabled={loading}
+                onPress={signInWithEmail}
+                className="mb-5 min-h-16 w-11/12 items-center justify-center rounded-lg font-bold dark:bg-lime-500">
+                {loading ? (
+                  <ActivityIndicator size="large" color="white" />
+                ) : (
+                  <Text className="text-2xl font-extrabold antialiased dark:text-zinc-200">
+                    Sign in
+                  </Text>
+                )}
+              </Pressable>
 
-            <Link
-              href="/sign-up"
-              className="mt-2 items-center text-lg font-bold dark:text-lime-500">
-              Create an account
-            </Link>
-
-          </View>
-        </BlurView>
-      </View>
+              <Link
+                href="/sign-up"
+                className="mt-2 items-center text-lg font-bold dark:text-lime-500">
+                Create an account
+              </Link>
+            </View>
+          </BlurView>
+        </View>
+      </BlurView>
     </ImageBackground>
   );
 };
--- components/CategoryListItem.tsx
+++ components/CategoryListItem.tsx
@@ -3,10 +3,20 @@ import { Link } from 'expo-router';
 import { useState } from 'react';
 import { View, Text, ImageBackground, Pressable } from 'react-native';
 import { Tables } from 'types';
-// import { Tables } from '~/database.types';
 
-const image = {
-  uri: 'https://th.bing.com/th/id/OIG2.WQoWQuuEZL4iNRkQceXu?pid=ImgGn',
+const categoryImages = {
+  motions: {
+    uri: '*******/storage/v1/object/sign/category-images/PIRmotionTBG-115.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYXRlZ29yeS1pbWFnZXMvUElSbW90aW9uVEJHLTExNS5wbmciLCJpYXQiOjE3MTU4NTAyODEsImV4cCI6MTc0NzM4NjI4MX0.7ED86TKveazBFWAlDmEKV0_vUK-FALqFUHadloFstYo&t=2024-05-16T09%3A04%3A19.475Z',
+  },
+  windows: {
+    uri: '*******/storage/v1/object/sign/category-images/DoorTBG-100.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYXRlZ29yeS1pbWFnZXMvRG9vclRCRy0xMDAucG5nIiwiaWF0IjoxNzE1ODUwMzE0LCJleHAiOjE3NDczODYzMTR9.EmeFZchq93i6eh3vFn7gDuyKfEE_kD-lM2SWjqLj4g8&t=2024-05-16T09%3A04%3A52.088Z',
+  },
+  doors: {
+    uri: '*******/storage/v1/object/sign/category-images/DoorTBG-100.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYXRlZ29yeS1pbWFnZXMvRG9vclRCRy0xMDAucG5nIiwiaWF0IjoxNzE1ODUwMzM3LCJleHAiOjE3NDczODYzMzd9.Y9WEK-lqRJrlIzAJxzzKBBvpaCPsKmmfkD_9uVgf_A0&t=2024-05-16T09%3A05%3A15.587Z',
+  },
+  alarms: {
+    uri: '*******/storage/v1/object/sign/category-images/AlarmTBG-1000g.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYXRlZ29yeS1pbWFnZXMvQWxhcm1UQkctMTAwMGcucG5nIiwiaWF0IjoxNzE1ODUwMzAwLCJleHAiOjE3NDczODYzMDB9.wRostGzVJ6s0NVWjmyN7HHHHnX8Fgk6tmP7PwcrGbnc&t=2024-05-16T09%3A04%3A38.821Z',
+  },
 };
 
 type CategoryListItemProps = {
@@ -35,7 +45,10 @@ const CategoryListItem = ({
       }}
       asChild>
       <Pressable>
-        <ImageBackground source={image} resizeMode="cover" imageStyle={{ borderRadius: 16 }}>
+        <ImageBackground
+          source={categoryImages[sensor.category as keyof typeof categoryImages]}
+          resizeMode="cover"
+          imageStyle={{ borderRadius: 16 }}>
           <View className="min-h-48 min-w-48 basis-1/4 rounded-2xl bg-black/15 backdrop-blur-sm dark:bg-black/45">
             <View className="flex-1 justify-between p-4">
               <View>
